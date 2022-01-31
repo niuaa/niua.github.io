9a650392d4d86980a6830a43de4a343420a470f4
niuaa <92570768+niuaa@users.noreply.github.com>
2022-01-31 22:18:27 +0800

    Update script.js

--- fireworks/script.js
+++ fireworks/script.js
@@ -2292,7 +2292,7 @@ if (IS_HEADER) {
 	init();
 } else {
 	// Allow status to render, then preload assets and start app.
-	setLoadingStatus('献给你爱的TA ❤');
+	setLoadingStatus('正在装配烟花');
 	setTimeout(() => {
 		soundManager.preload()
 		.then(
